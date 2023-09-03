38bb35eb4fe419de428f63c364b1bc23fcd5c574
Gleb Vorontsov <g.vorontcov@corp.vk.com>
2023-09-03 15:06:52 +0200

    actions config

---
+++ dist/assets/PageNotFound-18438faa.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-eebe1cff.js";const s=()=>t("div",{children:"This path is not exist!"});export{s as default};
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
    plugins: [react(), tsconfigPaths(), basicSsl()],
+   base: '/TWA-Playground/'
 });
